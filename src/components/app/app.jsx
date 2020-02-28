22791d4cb7f1f08e9cb30ce179f9893cc272f262
Washingto Pires <4washingtonbr@gmail.com>
2020-02-28 15:46:15 -0300

    Add theme provider of Styled Components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { ThemeProvider } from 'styled-components';
 import 'normalize.css';
+
+const theme = {
+  primaryColor: 'rgb(153, 153, 153)',
+  secondaryColor: 'rgb(250, 250, 250)',
+  hoverColor: 'rgba(0, 0, 0, 0.5)',
+  bgColor: 'rgb(29, 29, 29)',
+};
 
 function App() {
   return (
-    <>App component</>
+    <ThemeProvider theme={theme}>
+      App component
+    </ThemeProvider>
   );
 }
 
