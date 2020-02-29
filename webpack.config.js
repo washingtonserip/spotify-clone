5ee266f203389d3b2f3a6030ee85bfaee3f8fa77
Washingto Pires <4washingtonbr@gmail.com>
2020-02-29 17:29:50 -0300

    Add support for environment variable files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -47,5 +48,10 @@ module.exports = {
     hotOnly: true,
     historyApiFallback: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new Dotenv({
+      path: `./environments/.env.${process.env.NODE_ENV}`,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
