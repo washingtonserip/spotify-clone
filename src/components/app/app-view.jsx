01c6187382ead9a8ae8f45e7f0abec58c9b65610
Washingto Pires <4washingtonbr@gmail.com>
2020-02-29 15:46:07 -0300

    Add wrapper style to App component

--- src/components/app/app-view.jsx
+++ src/components/app/app-view.jsx
@@ -3,19 +3,23 @@ import { hot } from 'react-hot-loader/root';
 import { ThemeProvider } from 'styled-components';
 import 'normalize.css';
 import Routes from '../routes';
+import { Wrapper } from './app-styles';
 
 const theme = {
   primaryColor: 'rgb(153, 153, 153)',
   secondaryColor: 'rgb(250, 250, 250)',
+  greenColor: 'rgba(30, 215, 96, 1)',
   hoverColor: 'rgba(0, 0, 0, 0.5)',
   bgColor: 'rgb(29, 29, 29)',
 };
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <Wrapper>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </Wrapper>
   );
 }
 
