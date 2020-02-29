285f5becb49fd507d04fe31885b73f31d47dd637
Washingto Pires <4washingtonbr@gmail.com>
2020-02-29 11:57:57 -0300

    Set historyApiFallback on Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,7 @@ module.exports = {
     port: 3000,
     publicPath: 'http://localhost:3000/dist/',
     hotOnly: true,
+    historyApiFallback: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 };
