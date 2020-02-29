ce7fa8b8688f2c89d3b375afe0d3a310f978ea7d
Washingto Pires <4washingtonbr@gmail.com>
2020-02-29 12:14:29 -0300

    Add Home Component

---
+++ src/pages/home/home-view.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+function HomeView() {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  );
+}
+
+export default HomeView;
