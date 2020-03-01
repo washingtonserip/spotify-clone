ac2e708e76e6eaf4572f4e0bc25f19884f12fdbd
Washingto Pires <4washingtonbr@gmail.com>
2020-03-01 01:46:34 -0300

    Fix margin bottom on headings styles

--- src/components/headings/headings-styles.js
+++ src/components/headings/headings-styles.js
@@ -6,16 +6,18 @@ export const Title = styled.h1`
   font-weight: 400;
   line-height: 19px;
   color: ${(props) => props.theme.secondaryColor};
-  margin: 0;
+  margin: 0 0 16px;
   padding: 0;
 
   @media(min-width: 768px) {
-  font-size: 20px;
-  line-height: 23px;
+    font-size: 20px;
+    line-height: 23px;
+    margin-bottom: 32px;
   }
 
   @media(min-width: 992px) {
     font-size: 24px;
     line-height: 28px;
+    margin-bottom: 40px;
   }
 `;
