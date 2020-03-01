639a6aa22802a69dfaf6c7b2f6cc673fc9ef072f
Washingto Pires <4washingtonbr@gmail.com>
2020-03-01 13:08:14 -0300

    add album components to the home page

--- src/pages/home/home-view.jsx
+++ src/pages/home/home-view.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Layout from '../../components/layout';
 import Search from '../../components/search';
+import Headings from '../../components/headings';
+import AlbumList from '../../components/album-list';
 
 function HomeView() {
   return (
     <Layout>
       <Search />
 
-      Home
+      <Headings>
+        Álbuns buscados recentemente
+      </Headings>
+
+      <AlbumList />
+
+      <Headings>
+        Álbuns buscados recentemente
+      </Headings>
+
+      <AlbumList />
     </Layout>
   );
 }
