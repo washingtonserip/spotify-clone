83cfc7b8e719e6f7670559fe599e2a9760cff100
Washingto Pires <4washingtonbr@gmail.com>
2020-03-02 19:52:59 -0300

    Data binding on the search page

--- src/pages/search/search-container.jsx
+++ src/pages/search/search-container.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { saveCacheAlbums } from '../../redux/search-cache/actions';
+import { getCachedAlbumsByQuery } from '../../redux/search-cache/selectors';
+import SearchProvider from '../../providers/search';
 import SearchView from './search-view';
 
 function SearchContainer() {
+  const { query } = useParams();
+  const chachedAlbums = useSelector(getCachedAlbumsByQuery(query));
+  const dispatch = useDispatch();
+
+  function searchAlbuns() {
+    SearchProvider.searchV1(query)
+      .then((res) => res.json())
+      .then((res) => dispatch(saveCacheAlbums(query, res.albums)));
+  }
+
+  function verifyCacheAndFetchAlbums() {
+    if (query && !chachedAlbums) {
+      searchAlbuns();
+    }
+  }
+
+  verifyCacheAndFetchAlbums();
+
   return (
-    <SearchView />
+    <SearchView
+      query={query || ''}
+      results={chachedAlbums && chachedAlbums.items ? chachedAlbums.items : []}
+    />
   );
 }
 
--- src/pages/search/search-view.jsx
+++ src/pages/search/search-view.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../../components/layout';
 import Search from '../../components/search';
 import Headings from '../../components/headings';
 import SearchResults from './components/search-results';
 
-function SearchView() {
+function SearchView({
+  query,
+  results,
+}) {
+  const title = results.lenght ? 'Resultados encontrados' : 'Nenhum resultado encontrado';
   return (
     <Layout>
-      <Search />
+      <Search
+        query={query}
+      />
 
-      <Headings>
-        Resultados encontrados para &quot;Bob&quot;
-      </Headings>
+      {query && (
+        <>
+          <Headings>
+            {`${title} para "${decodeURIComponent(query)}"`}
+          </Headings>
 
-      <SearchResults />
+          <SearchResults
+            results={results}
+          />
+        </>
+      )}
     </Layout>
   );
 }
+
+SearchView.propTypes = {
+  query: PropTypes.string,
+  results: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+SearchView.getDefaultProps = {
+  query: '',
+};
 
 export default SearchView;
