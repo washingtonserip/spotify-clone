6121369e1a0e7c3b075cd44d730e4a51976f0d51
Washingto Pires <4washingtonbr@gmail.com>
2020-03-03 00:53:01 -0300

    Data binding on album list component

--- src/components/album-list/album-list-view.jsx
+++ src/components/album-list/album-list-view.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AlbumThumbnail from '../album-thumbnail';
 import { Wrapper } from './album-list-styles';
 
-function AlbumListView() {
+function AlbumListView({ albums }) {
   return (
     <Wrapper>
-      {[0, 1, 2, 3, 4].map(() => <AlbumThumbnail />)}
+      {albums.map((album) => (
+        <AlbumThumbnail
+          key={album.id}
+          id={album.id}
+          cover={album.images[0].url}
+          name={album.name}
+          artists={album.artists.map((artist) => artist.name)}
+        />
+      ))}
     </Wrapper>
   );
 }
+
+AlbumListView.propTypes = {
+  albums: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default AlbumListView;
