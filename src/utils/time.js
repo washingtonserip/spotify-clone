ab8204b45e8035f4c9f8951c33a276882946d3fb
Washingto Pires <4washingtonbr@gmail.com>
2020-03-03 00:13:33 -0300

    Add millisToMinutesAndSeconds helper

---
+++ src/utils/time.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export function millisToMinutesAndSeconds(millis) {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`;
+}
