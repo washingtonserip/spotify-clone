1e0a3db4d73d48ac9d928b4802f073a848747abc
Washingto Pires <4washingtonbr@gmail.com>
2020-03-03 00:28:28 -0300

    Add default bg to app

--- src/components/app/app-styles.js
+++ src/components/app/app-styles.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 // eslint-disable-next-line import/prefer-default-export
 export const Wrapper = styled.div`
   font-family: 'Roboto', sans-serif;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.bgColor};
 `;
