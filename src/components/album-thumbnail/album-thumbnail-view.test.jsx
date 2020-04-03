69c6983e89fda2afe7d407914b3360bc05a3cae0
Washingto Pires <4washingtonbr@gmail.com>
2020-04-03 13:38:19 -0300

    Update mock for testing on AlbumThumbnailView component

--- src/components/album-thumbnail/album-thumbnail-view.test.jsx
+++ src/components/album-thumbnail/album-thumbnail-view.test.jsx
@@ -9,10 +9,10 @@ describe('AlbumThumbnailView', () => {
       .create((
         <MemoryRouter>
           <AlbumThumbnailView
-            id="XPTO"
-            cover="XPTO"
-            name="XPTO"
-            artists={['XPTO']}
+            id="4otkd9As6YaxxEkIjXPiZ6"
+            cover="https://i.scdn.co/image/ab67616d0000b2732f44aec83b20e40f3baef73c"
+            name="Music To Be Murdered By"
+            artists={['Eminem']}
           />
         </MemoryRouter>
       ))
