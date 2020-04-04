064868268be96208e04e322684eea83ce01dbaaf
Washingto Pires <4washingtonbr@gmail.com>
2020-04-04 13:39:37 -0300

    Add test to the AlbumListView component

---
+++ src/components/album-list/album-list-view.test.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+import AlbumListView from './album-list-view';
+import AlbumThumbnail from '../album-thumbnail';
+
+const albumsMock = [
+  {
+    artists: [
+      {
+        name: 'Eminem',
+      },
+    ],
+    id: '4otkd9As6YaxxEkIjXPiZ6',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b2732f44aec83b20e40f3baef73c',
+        width: 640,
+      },
+    ],
+    name: 'Music To Be Murdered By',
+  },
+  {
+    album_type: 'album',
+    artists: [
+      {
+        name: 'Eminem',
+      },
+    ],
+    id: '2cWBwpqMsDJC1ZUwz813lo',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b2736ca5c90113b30c3c43ffb8f4',
+        width: 640,
+      },
+    ],
+    name: 'The Eminem Show',
+  },
+  {
+    album_type: 'album',
+    artists: [
+      {
+        name: 'Eminem',
+      },
+    ],
+    id: '47BiFcV59TQi2s9SkBo2pb',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b273c08d5fa5c0f1a834acef5100',
+        width: 640,
+      },
+    ],
+    name: 'Recovery',
+  },
+  {
+    album_type: 'album',
+    artists: [
+      {
+        name: 'Eminem',
+      },
+    ],
+    id: '3HNnxK7NgLXbDoxRZxNWiR',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b273e4073def0c03a91e3fceaf73',
+        width: 640,
+      },
+    ],
+    name: 'Kamikaze',
+  },
+  {
+    album_type: 'album',
+    artists: [
+      {
+        name: 'Eminem',
+      },
+    ],
+    id: '6t7956yu5zYf5A829XRiHC',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab67616d0000b273dbb3dd82da45b7d7f31b1b42',
+        width: 640,
+      },
+    ],
+    name: 'The Marshall Mathers LP',
+  },
+];
+
+describe('AlbumListView', () => {
+  test('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <AlbumListView
+            albums={albumsMock}
+          />
+        </MemoryRouter>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should display the AlbumThumbnail component 5 times', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <AlbumListView
+          albums={albumsMock}
+        />
+      </MemoryRouter>,
+    );
+
+    expect(wrapper.find(AlbumThumbnail)).toHaveLength(albumsMock.length);
+  });
+});
