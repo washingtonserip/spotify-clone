6bb02ec8a04119f00fb4979bffc6cc8a5083a25a
Washingto Pires <4washingtonbr@gmail.com>
2020-04-06 12:41:21 -0300

    Add test to the LayoutView component

---
+++ src/components/layout/layout-view.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter, Link } from 'react-router-dom';
+import { mount } from 'enzyme';
+import LayoutView from './layout-view';
+
+describe('LayoutView', () => {
+  test('render corectly', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <LayoutView>Content</LayoutView>
+        </MemoryRouter>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('includes link to home screen', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <LayoutView>Content</LayoutView>
+      </MemoryRouter>,
+    );
+
+    expect(wrapper.find(Link).props().to).toBe('/');
+  });
+});
