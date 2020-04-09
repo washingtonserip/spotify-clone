2f7a1d5eb70588cfa577da20120bbd67cb349330
Washingto Pires <4washingtonbr@gmail.com>
2020-04-08 22:51:01 -0300

    Create theme file

---
+++ src/components/app/app-theme.js
@@ -0,0 +1,7 @@
+export default {
+  primaryColor: 'rgb(153, 153, 153)',
+  secondaryColor: 'rgb(250, 250, 250)',
+  greenColor: 'rgba(30, 215, 96, 1)',
+  hoverColor: 'rgba(0, 0, 0, 0.5)',
+  bgColor: 'rgb(29, 29, 29)',
+};
--- src/components/app/app-view.jsx
+++ src/components/app/app-view.jsx
@@ -3,14 +3,7 @@ import 'normalize.css';
 import { ThemeProvider } from 'styled-components';
 import Routes from '../routes';
 import { Wrapper } from './app-styles';
-
-const theme = {
-  primaryColor: 'rgb(153, 153, 153)',
-  secondaryColor: 'rgb(250, 250, 250)',
-  greenColor: 'rgba(30, 215, 96, 1)',
-  hoverColor: 'rgba(0, 0, 0, 0.5)',
-  bgColor: 'rgb(29, 29, 29)',
-};
+import theme from './app-theme';
 
 function AppView() {
   return (
