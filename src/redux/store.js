9027dbfb29d5a73c7714b51226084c0d0ccdfd11
Washingto Pires <4washingtonbr@gmail.com>
2020-04-14 15:42:00 -0300

    Add and configure Redux Thunk dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   rootReducer,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancer(applyMiddleware(thunk)),
 );
